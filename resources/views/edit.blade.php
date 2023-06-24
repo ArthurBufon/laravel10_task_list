819ec8c198bf2714f9904579ec8e0667920ffa51
Arthur <arthurbufon2052@gmail.com>
2023-06-24 17:22:27 -0300

    route model binding e formRequest

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,30 +12,26 @@
 @endsection
 
 @section('content')
-    <form action="{{ route('tasks.update', ['id' => $task->id]) }}" method="POST">
+    <form action="{{ route('tasks.update', ['task' => $task->id]) }}" method="POST">
         @csrf
         @method('PUT')
         <div>
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" value="{{$task->title}}">
+            <input type="text" name="title" id="title" value="{{ $task->title }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="description">Description</label>
-            <textarea name="description" id="description">
-                {{$task->description}}
-            </textarea>
+            <textarea name="description" id="description">{{ $task->description }}</textarea>
             @error('description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="long_description">Long Description</label>
-            <textarea name="long_description" id="long_description">
-                {{$task->long_description}}
-            </textarea>
+            <textarea name="long_description" id="long_description">{{ $task->long_description }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
