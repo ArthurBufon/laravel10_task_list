1e163b58d34ec15b4afa7e34d92f5638c2e5024a
Arthur <arthurbufon2052@gmail.com>
2023-06-28 19:40:46 -0300

    deletando tasks

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,17 @@ Route::get('/', function(){
     return redirect()->route('tasks.index');
 });
 
-// All tasks.
+// All Tasks.
 Route::get('/tasks', function(){
     return view('index', [
         'tasks' => Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
-// Form create task.
+// Form create Task.
 Route::view('tasks/create', 'create')->name('tasks.create');
 
-// Show task.
+// Show Task.
 Route::get('/tasks/{task}', function(Task $task){
     return view('show', ['task' => $task]);
 })->name('tasks.show');
@@ -29,20 +29,28 @@ Route::get('/tasks/{task}/edit', function(Task $task){
     return view('edit', ['task' => $task]);
 })->name('tasks.edit');
 
-// New task.
+// New Task.
 Route::post('/tasks', function(TaskRequest $request){
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-// Update Task
+// Update Task.
 Route::put('/tasks/{task}', function(Task $task, TaskRequest $request){
     $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+// Delete Task.
+Route::delete('/tasks/{task}', function(Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
+// Route not found.
 Route::fallback(function () {
     return 'Still got somewhere!';
 });
