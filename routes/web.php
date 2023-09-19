0e85f88f627eaff356fbf0d0a3b2f339a8a16de0
Arthur <arthurbufon2052@gmail.com>
2023-09-18 22:06:44 -0300

    Testing if admin can update tasks

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::middleware('admin')->group(function () {
     Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
         $task->update($request->validated());
 
-        return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully!');
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
     })->name('tasks.update');
 
     // Delete Task.
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -44,11 +44,10 @@ class TaskTest extends TestCase
         ]);
     }
 
-
     /**
      * Test if admin can create a new task.
      */
-    public function test_admin_can_see_the_edit_product_page()
+    public function test_admin_can_see_the_edit_task_page()
     {
         // Creates new admin user.
         $admin = User::factory()->create(['is_admin' => 1]);
@@ -57,12 +56,72 @@ class TaskTest extends TestCase
         $task = Task::factory()->create();
 
         // Sends get request to edit task route.
-        $response = $this->actingAs($admin)->get('/tasks/' . $task->id. '/edit');
+        $response = $this->actingAs($admin)->get('/tasks/' . $task->id . '/edit');
 
         // Verifies if response status is 200.
         $response->assertStatus(200);
 
         // Verifying if edit page contains the title of the created task.
         $response->assertSee($task->title);
+    }
+
+    /**
+     * Test if admin can update a task
+     */
+    public function test_admin_can_update_task()
+    {
+        // Creates new admin user.
+        $admin = User::factory()->create(['is_admin' => 1]);
+
+        // Creates new task.
+        Task::factory()->create();
+
+        // Asserts that there is at least 1 task in table.
+        $this->assertCount(1, Task::all());
+
+        // Gets the first task.
+        $task = Task::first();
+
+        // Sends put request to update task route.
+        $response = $this->actingAs($admin)->put('/tasks/' . $task->id, [
+            'title' => 'UPDATED Learn Programming',
+            'description' => 'UPDATED Learn Unit Test with PHPUnit',
+            'long_description' => 'UPDATED Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]);
+
+        // Asserts no errors occured.
+        $response->assertSessionHasNoErrors();
+
+        // Asserts the redirect.
+        $response->assertRedirect('/tasks');
+
+        // Asserts that the data of the task has been updated successfully.
+        $this->assertEquals('UPDATED Learn Programming', Task::first()->title);
+        $this->assertEquals('UPDATED Learn Unit Test with PHPUnit', Task::first()->description);
+        $this->assertEquals('UPDATED Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', Task::first()->long_description);
+    }
+
+    /**
+     * Test if admin can delete a task
+     */
+    public function test_admin_can_delete_task()
+    {
+        // Creates new admin user.
+        $admin = User::factory()->create(['is_admin' => 1]);
+
+        // Creates new task.
+        $task = Task::factory()->create();
+
+        // Asserts that there is at least 1 task in table.
+        $this->assertEquals(1, Task::count());
+
+        // Sends delete request to delete task route.
+        $response = $this->actingAs($admin)->delete('/tasks/'. $task->id);
+
+        // Asserts response status is 302 (deleted successfully).
+        $response->assertStatus(302);
+
+        // Asserts tasks table is empty
+        $this->assertEquals(0, Task::count());
     }
 }
